4f3948e12ac7dcfe17f27c12f016b9140650a682
Michael Stonis <michael.stonis@eightbot.com>
2019-05-06 08:17:56 -0500

    add mobile applicaiton

---
+++ OrbitSample/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using EightBot.Orbit.Client;
+using Splat;
+
+namespace OrbitSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var orbitClient =
+                new OrbitClient()
+                    .Initialize(Xamarin.Essentials.FileSystem.AppDataDirectory)
+                    .AddTypeRegistration<Post, long>(x => x.Id)
+                    .AddTypeRegistration<User, long>(x => x.Id);
+
+            Locator.CurrentMutable.RegisterConstant<OrbitClient>(orbitClient);
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ OrbitSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using EightBot.Orbit.Client;
+using Splat;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace OrbitSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var client = Locator.Current.GetService<OrbitClient>();
+
+            var httpClient = new HttpClient();
+            var usersJson = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(usersJson);
+            client.PopulateCache(users);
+
+            var cachedUsers = client.GetLatest<User>();
+
+            listView.ItemsSource = cachedUsers;
+        }
+    }
+}
